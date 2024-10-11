cdd1f44a52cfd3c52d640554744f0e0cfc8f0b41
Sukonmi <danielayomideh@gmail.com>
2024-10-11 11:35:44 +0100

    I edited the python checkpoint code

--- checkpoint.py
+++ checkpoint.py
@@ -1,24 +1,24 @@
-print("Welcome to Python Pizza Deliveries")
+print("Welcome to Ayomide's Python Pizza Deliveries")
 
 # Define pizza prices
-print("pizza_size: Small, Medium, Large")
+print("Pizza sizes: Small, Medium, Large")
 pizza_size = input("Enter pizza size: \n")
 
 # Calculate pizza cost
-if pizza_size == "Small":
+if pizza_size.lower() == "small":
     pizza_cost = 15
-elif pizza_size == "Medium":
+elif pizza_size.lower() == "medium":
     pizza_cost = 20
 else:
     pizza_cost = 25
 
 # Ask for pepperoni
 print("Add pepperoni? (yes/no)")
-pepperoni = input()
+pepperoni = input().lower()
 if pepperoni == 'yes':
-    if pizza_size == "Small":
+    if pizza_size.lower() == "small":
         pepperoni_cost = 2
-    elif pizza_size == "Medium":
+    elif pizza_size.lower() == "medium":
         pepperoni_cost = 3
     else:
         pepperoni_cost = 5
@@ -28,7 +28,7 @@ else:
 # Ask for cheese
 cheese_price = 5
 print("Add cheese? (yes/no)")
-cheese = input()
+cheese = input().lower()
 if cheese == 'yes':
     cheese_cost = cheese_price
 else:
@@ -38,13 +38,13 @@ else:
 total_cost = pizza_cost + pepperoni_cost + cheese_cost
 
 # Display order summary
-print("\nOrder Summary: ")
-print("Pizza (" + pizza_size + "): $" + str(pizza_cost))
+print("\nOrder Summary:")
+print(f"Pizza ({pizza_size}): ${pizza_cost}")
 if pepperoni_cost > 0:
-    print("Pepperoni (" + pizza_size + "): $" + str(pepperoni_cost))
+    print(f"Pepperoni ({pizza_size}): ${pepperoni_cost}")
 if cheese_cost > 0:
-    print("Cheese: $" + str(cheese_cost))
-print("Total: $" + str(total_cost))
+    print(f"Cheese: ${cheese_cost}")
+print(f"Total: ${total_cost}")
 
 print("Thank you for your patronage.")
 
